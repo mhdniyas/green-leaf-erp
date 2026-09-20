bc2e0fd8b9f8444445594ba897bd3dd4b9f8fa46
Niyas Mohammed <niyas@example.com>
2026-09-20 14:18:25 +0530

    zoho

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    'zoho' => [
+        'client_id' => env('ZOHO_CLIENT_ID'),
+        'client_secret' => env('ZOHO_CLIENT_SECRET'),
+        'redirect_uri' => env('ZOHO_REDIRECT_URI'),
+    ],
+
 ];
